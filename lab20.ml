c184415ac6a5612c39b91b830783f5e77cc59cdc
Yasmin Luthra <yasminluthra@Yasmins-MacBook-Pro-2.local>
2020-04-23 09:59:59 -0700

    done

--- lab20.ml
+++ lab20.ml
@@ -2,7 +2,6 @@
 (* Lab 20: Filtering the Mona Lisa *)
 
 open Graphics ;;
-open Monalisa ;;
 
 (* images are lists of lists of floats between 0. (white) and 1. (black) *)
 type image = float list list ;;
@@ -11,24 +10,24 @@ type size = int * int ;;
 
 (* threshold threshold image -- image where pixels above the threshold
 value are black *)
-let threshold img threshold =
+let threshold (img : image) (threshold : float) =
   List.map (fun row -> List.map (fun pix -> if pix <= threshold then 0. else 1.)
                                  row) img
 ;;
 
 (* dither max image: makes each pixel black or white based on its rgb value *)
-let dither img =
+let dither (img : image) =
   List.map(fun row -> List.map (fun pix -> if pix > Random.float 1.
                                            then 1. else 0.) row) img
 ;;
 
 (* show the image *)
-let depict img =
+let depict (img : image) =
   open_graph "";
   clear_graph ();
   let x, y = List.length (List.hd img), List.length img in
   resize_window x y;
-  let depict_pix v r c =
+  let depict_pix (v : float) (r : int) (c : int) =
     let lvl = int_of_float (255. *. (1. -. v)) in
     set_color (rgb lvl lvl lvl);
     plot c (y - r) in
@@ -42,7 +41,7 @@ let depict img =
 
 (* print images *)
 let _ =
-  depict image;
-  depict (dither image);
-  depict (threshold image 0.75)
+  depict Monalisa.image;
+  depict (dither Monalisa.image);
+  depict (threshold Monalisa.image 0.75)
 ;;
